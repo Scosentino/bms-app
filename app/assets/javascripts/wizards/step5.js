3e7f0fbe8f9f9afa3842ab2e917b1fc7b7f13683
Nikolay Kuzmenko <kuzmenkojob@gmail.com>
2018-05-19 18:25:03 +0300

    add validation for step 5

---
+++ app/assets/javascripts/wizards/step5.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $("#new_user_wizard").validate({
+        highlight: function(element, errorClass, validClass) {
+            $(element).addClass(errorClass).removeClass(validClass);
+            $(element.form).find("label[for=" + element.id + "]")
+                .addClass(errorClass)
+        },
+        unhighlight: function(element, errorClass, validClass) {
+            $(element).removeClass(errorClass).addClass(validClass);
+            var text = $("label[for=" + element.id + "]").data('old_text');
+            $(element.form).find("label[for=" + element.id + "]")
+                .removeClass(errorClass)
+                .attr('style', 'display: inline-block;')
+                .text(text);
+        },
+        rules: {
+            'user_wizard[orders][accepted_policy]': {
+                required: true
+            }
+        },
+        messages: {
+            'user_wizard[orders][accepted_policy]': "Please Accept Policy"
+        }
+    });
+});
