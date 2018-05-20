48d30a44eb89b6da4eae403969f5fbcdae9479bc
Nikolay Kuzmenko <kuzmenkojob@gmail.com>
2018-05-20 12:40:12 +0300

    remove create order from nav

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,12 @@ class HomeController < ApplicationController
   layout 'application'
 
   def index
-
+    if current_user.present?
+      if current_user.admin?
+        redirect_to admins_path
+      elsif current_user.customer?
+        redirect_to customers_path
+      end
+    end
   end
 end
