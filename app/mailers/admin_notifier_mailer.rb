f4f3c1c67fff1ccab2dcfa0619b6297a2a8d673d
Nikolay Kuzmenko <kuzmenkojob@gmail.com>
2018-05-20 03:20:42 +0300

    little fix

--- app/mailers/admin_notifier_mailer.rb
+++ app/mailers/admin_notifier_mailer.rb
@@ -10,16 +10,16 @@ class AdminNotifierMailer < ApplicationMailer
       @order = Order.find_by(id: order_id)
 
       if @order.present?
-        pdf = render_to_string_with_wicked_pdf(
-            pdf: 'merchant_agreement',
-            :template => 'generate_contracts/merchant_agreement',
-            :locals => {:@model => @order, :@customer => @customer },
-            :margin => { :bottom => 10 },
-            :footer => { :html => { :template => 'generate_contracts/footer', :locals => {:@model => @order, :@customer => @customer }} }
-        )
-        attachments["merchant_agreement_#{@order.id}.pdf"] = pdf
-
         @email.each do |e|
+          pdf = render_to_string_with_wicked_pdf(
+              pdf: 'merchant_agreement',
+              :template => 'generate_contracts/merchant_agreement',
+              :locals => {:@model => @order, :@customer => @customer },
+              :margin => { :bottom => 10 },
+              :footer => { :html => { :template => 'generate_contracts/footer', :locals => {:@model => @order, :@customer => @customer }} }
+          )
+          attachments["merchant_agreement_#{@order.id}.pdf"] = pdf
+
           mail(to: e,
                subject: "#{@customer.full_name} created new Order")
         end
