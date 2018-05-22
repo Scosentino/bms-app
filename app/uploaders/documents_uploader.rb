b98b4325ea6a4bc0f5fcd9d3b49f7c35e56f49fd
Nikolay Kuzmenko <kuzmenkojob@gmail.com>
2018-05-23 02:22:52 +0300

    tmp

--- app/uploaders/documents_uploader.rb
+++ app/uploaders/documents_uploader.rb
@@ -1,7 +1,3 @@
 class DocumentsUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
-
-  def public_id
-    "#{model.class.to_s.underscore}-#{mounted_as}-#{model.id}"
-  end
 end
